1726cde4ef9a8dcb1d4071519610d2c6d4e50435
Brant Young <anatman.xx@gmail.com>
2014-11-04 17:53:53 +0800

    add test case.

--- src/test/java/com/sky/zookeeper/ZooKeeperAnnotationTest.java
+++ src/test/java/com/sky/zookeeper/ZooKeeperAnnotationTest.java
@@ -1,5 +1,7 @@
 package com.sky.zookeeper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
@@ -11,12 +13,53 @@ import com.sky.zookeeper.type.SubscribeType;
 
 @ZkManage
 @Component
-public class ZooKeeperAnnotationTest {
-	@ZkValue(value = "", subscribeType = SubscribeType.DATA_CHANGE, createStrategy = CreateStrategy.CONSTRUCTOR)
-	private String value;
+public class ZooKeeperAnnotationTest extends ZkContext {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ZooKeeperAnnotationTest.class);
+
+	@ZkValue(value = "/sms/phoneProvider", createStrategy = CreateStrategy.CONSTRUCTOR, subscribeType = SubscribeType.DATA_CHANGE)
+	public void setProvider(String provider) {
+		LOGGER.info("method called with argument \"" + provider + "\"");
+	}
+	
+	@ZkValue(value = "/sms/phoneProvider", createStrategy = CreateStrategy.CONSTRUCTOR, subscribeType = SubscribeType.DATA_CHANGE)
+	private String phone;
+
+	@Override
+	public String getZkConnection() {
+		return "localhost:2181";
+	}
+
+	@Override
+	public Integer getZkConnectionTimeout() {
+		return 1000;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
 
 	public static void main(String[] args) {
+		LOGGER.info("initial...");
+		
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-zookeeper.xml");
-		applicationContext.getBean("");
+		ZooKeeperAnnotationTest bean = (ZooKeeperAnnotationTest) applicationContext.getBean("zooKeeperAnnotationTest");
+		
+		LOGGER.info("prev phone value " + bean.getPhone());
+
+		LOGGER.info("sleep...");
+		try {
+			Thread.currentThread().sleep(10000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		LOGGER.info("change zk data...");
+		try {
+			bean.getZkClient().setData().forPath("/sms/phoneProvider", "123321".getBytes());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		LOGGER.info("current phone value " + bean.getPhone());
 	}
 }
